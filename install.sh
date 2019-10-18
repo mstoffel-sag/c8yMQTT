9eccd78eb26dbb87a62f5ff1b91c4b147571daea
Marco Stoffel <marco.stoffel@softwareag.com>
2019-10-18 09:10:02 +0200

    chown wrong order fixed

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 mkdir -p /opt/softwareag/c8yPyAgent
-chown -R pi /opt/softwareag
 cp piAgent.py c8yMQTT.py pi.properties /opt/softwareag/c8yPyAgent
 sudo cp c8y.service  /etc/systemd/system/
+chown -R pi /opt/softwareag
 sudo systemctl enable c8y.service 
 sudo systemctl daemon-reload
 sudo service c8y restart

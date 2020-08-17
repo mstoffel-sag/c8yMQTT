cc3d92c64d076ab6131478589d5abda67165d822
Marco Stoffel <marco.stoffel@softwareag.com>
2020-08-17 13:07:42 +0100

    Adding pip install support

--- install.sh
+++ install.sh
@@ -14,6 +14,10 @@ ExecStart=/usr/bin/python3 $(pwd)/piAgent.py
 [Install]
 WantedBy=multi-user.target" > c8y.service
 
+
+python3 -m pip install --upgrade pip
+python3 -m pip install -r ./requirements.txt
+
 sudo cp c8y.service  /etc/systemd/system/
 sudo systemctl enable c8y.service 
 sudo systemctl daemon-reload
